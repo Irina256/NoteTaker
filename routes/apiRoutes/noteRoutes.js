be92421fe910e06d648866d664da36c883e56207
Irina Skitayeva <irina.skitayeva@gmail.com>
2021-01-17 17:43:21 -0500

    added functions to edit and delete notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,11 +1,41 @@
 const router = require("express").Router();
 const { notes } = require("../../Develop/db/db.json");
 const {
-  createNote,
+  createNewNote,
   editNote,
   deleteNote,
-  findById,
+  findNoteById,
 } = require("../../lib/notes");
 const uniqid = require("uniqid");
+
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+router.get("/notes/:id", (req, res) => {
+  const result = findNoteById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post("/notes", (req, res) => {
+  if (!req.body.id) {
+    req.body.id = uniqid();
+    createNewNote(req.body, notes);
+  } else {
+    editNote(req.body, notes);
+  }
+  res.json(req.body);
+});
+
+router.delete("/notes/:id", (req, res) => {
+  const result = findById(req.params.id, notes);
+
+  deleteNote(result, notes);
+  res.json();
+});
 
 module.exports = router;
